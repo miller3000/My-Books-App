1a85641983c82ce76ad57584e7e47fea656e803c
Miller 3000 <miller3000@users.noreply.github.com>
2018-04-13 12:04:25 -0500

    docs: Minor docs changes

--- src/index.js
+++ src/index.js
@@ -22,19 +22,18 @@
 */
 
 /**
-* @description
 * Renders React-DOM within React-Router.
-* More app information provided in App.js.
+* More information provided in README and in App.js.
 */
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router } from 'react-router-dom'
-
-//PASS LOCATION (PATHNAME) HISTORY TO APP.JS
 import createBrowserHistory from 'history/createBrowserHistory';
 import App from './App.js'
 import './index.css'
 
+//PASS LOCATION (PATHNAME) HISTORY TO APP.JS
 const history = createBrowserHistory();
 
 ReactDOM.render(
